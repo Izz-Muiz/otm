262d14e610c42bc16b4b8418701b5e6cad707a35
Izz-Muiz <muizzuddin0211@gmail.com>
2024-12-02 03:44:35 +0800

    feat: login

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -30,4 +30,16 @@ class User_model extends CI_Model
 
 		return true;
 	}
+
+	public function login($username)
+	{
+		$this->db->where('username', $username);
+		$query = $this->db->get('users');
+
+		if ($query->num_rows() > 0) {
+			return $query->row();
+		} else {
+			return false;
+		}
+	}
 }
